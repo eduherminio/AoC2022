273cf528055735b51f5fdb15882199a4a9c08e0b
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2022-11-13 23:31:31 +0100

    Initial commit

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,18 @@
+﻿if (args.Length == 0)
+{
+    Solver.SolveLast(opt => opt.ClearConsole = false);
+}
+else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
+{
+    Solver.SolveAll(opt =>
+    {
+        opt.ShowConstructorElapsedTime = true;
+        opt.ShowTotalElapsedTimePerDay = true;
+    });
+}
+else
+{
+    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+}
