04f85ac6c01526692a426b26937a54f7097d26c1
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-12-01 14:04:59 +0100

    Optimize Day 1 constructor

--- src/AoC_2022/Day_01.cs
+++ src/AoC_2022/Day_01.cs
@@ -6,15 +6,13 @@ public class Day_01 : BaseDay
 
     public Day_01()
     {
-        _input = ParseInput()
-            .Select(strList => strList.Select(str => int.Parse(str)))
-            .Select(intList => intList.Sum())
-            .ToList();
+        _input = ParsedFile
+            .ReadAllGroupsOfLines(InputFilePath)
+            .ConvertAll(strList => strList.Sum(str => int.Parse(str)))
+;
     }
 
     public override ValueTask<string> Solve_1() => new($"{_input.Max()}");
 
     public override ValueTask<string> Solve_2() => new($"{_input.OrderByDescending(n => n).Take(3).Sum()}");
-
-    private List<List<string>> ParseInput() => ParsedFile.ReadAllGroupsOfLines(InputFilePath);
 }
