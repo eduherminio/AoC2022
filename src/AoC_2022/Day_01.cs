21c1daf98c5b944a63cb4a158ba564b772d65245
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-12-01 11:19:31 +0100

    Use arrow methods where possible

--- src/AoC_2022/Day_01.cs
+++ src/AoC_2022/Day_01.cs
@@ -12,18 +12,9 @@ public class Day_01 : BaseDay
             .ToList();
     }
 
-    public override ValueTask<string> Solve_1()
-    {
-        return new($"{_input.Max()}");
-    }
+    public override ValueTask<string> Solve_1() => new($"{_input.Max()}");
 
-    public override ValueTask<string> Solve_2()
-    {
-        return new($"{_input.OrderByDescending(n => n).Take(3).Sum()}");
-    }
+    public override ValueTask<string> Solve_2() => new($"{_input.OrderByDescending(n => n).Take(3).Sum()}");
 
-    private List<List<string>> ParseInput()
-    {
-        return ParsedFile.ReadAllGroupsOfLines(InputFilePath);
-    }
+    private List<List<string>> ParseInput() => ParsedFile.ReadAllGroupsOfLines(InputFilePath);
 }
