cb5f56604d2192f3ab7f2bc522afb7fdfe7f5b6a
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-12-03 14:45:56 +0100

    Day 3: get rid of spans for part 2

---
+++ src/AoC_2022.Benchmarks/Day_03_Benchmark.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AoC_2022.Benchmarks;
+
+public class Day_03_Part1 : BaseDayBenchmark
+{
+    private readonly Day_03 _problem = new();
+
+    [Benchmark(Baseline = true)]
+    public async Task<string> Part1() => await _problem.Solve_1();
+
+    [Benchmark]
+    public async Task<string> WithoutSpan() => await _problem.Solve_1_WithoutSpan();
+}
+
+public class Day_03_Part2 : BaseDayBenchmark
+{
+    private readonly Day_03 _problem = new();
+
+    [Benchmark(Baseline = true)]
+    public async Task<string> Part2() => await _problem.Solve_2();
+
+    [Benchmark]
+    public async Task<string> WithSpan() => await _problem.Solve_2_WithSpan();
+}
--- src/AoC_2022/Day_03.cs
+++ src/AoC_2022/Day_03.cs
@@ -37,6 +37,70 @@ public class Day_03 : BaseDay
 
         for (int i = 0; i < _input.Length - 2; i += 3)
         {
+            var first = _input[i];
+            var second = _input[i + 1];
+            var third = _input[i + 2];
+
+            int index = 0;
+            while (true)
+            {
+                var item = first[index];
+                if (second.Contains(item) && third.Contains(item))
+                {
+                    break;
+                }
+                index++;
+            }
+
+            result += CalculatePriority(first[index]);
+        }
+
+        return new($"{result}");
+    }
+
+    /// <summary>
+    /// Significantly efficient
+    /// |      Method |     Mean |    Error |   StdDev | Ratio | RatioSD |    Gen0 | Allocated | Alloc Ratio |
+    /// |------------ |---------:|---------:|---------:|------:|--------:|--------:|----------:|------------:|
+    /// |       Part1 | 15.95 us | 0.240 us | 0.200 us |  1.00 |    0.00 |  0.0305 |     104 B |        1.00 |
+    /// | WithoutSpan | 25.76 us | 0.491 us | 0.546 us |  1.62 |    0.04 | 16.3879 |   34312 B |      329.92 |
+    /// </summary>
+    /// <returns></returns>
+    public ValueTask<string> Solve_1_WithoutSpan()
+    {
+        int result = 0;
+
+        foreach (var rucksack in _input)
+        {
+            var firstHalf = rucksack[..(rucksack.Length / 2)];
+            var secondHalf = rucksack[(rucksack.Length / 2)..];
+
+            int index = 0;
+            while (!secondHalf.Contains(firstHalf[index]))
+            {
+                index++;
+            }
+
+            result += CalculatePriority(firstHalf[index]);
+        }
+
+        return new($"{result}");
+    }
+
+    /// <summary>
+    /// Slightly less efficient
+    /// |      Method |     Mean |    Error |   StdDev | Ratio | RatioSD |   Gen0 | Allocated | Alloc Ratio |
+    /// |------------ |---------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
+    /// |    WithSpan | 13.87 us | 0.135 us | 0.126 us |  1.00 |    0.00 | 0.0458 |     104 B |        1.00 |
+    /// |       Part2 | 12.45 us | 0.199 us | 0.204 us |  0.90 |    0.02 | 0.0458 |     104 B |        1.00 |
+    /// </summary>
+    /// <returns></returns>
+    public ValueTask<string> Solve_2_WithSpan()
+    {
+        int result = 0;
+
+        for (int i = 0; i < _input.Length - 2; i += 3)
+        {
             var first = _input[i].AsSpan();
             var second = _input[i + 1].AsSpan();
             var third = _input[i + 2].AsSpan();
