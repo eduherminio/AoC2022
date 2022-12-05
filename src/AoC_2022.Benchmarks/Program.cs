0eda8c2380b5a2028fe257a49fafa1386e35f3ea
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-12-05 22:31:50 +0100

    Bump FileParser, cleaning

--- src/AoC_2022.Benchmarks/Program.cs
+++ src/AoC_2022.Benchmarks/Program.cs
@@ -2,17 +2,8 @@
 using BenchmarkDotNet.Running;
 using System.Reflection;
 
-namespace AoC_2020.Benchmarks
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
 #if DEBUG
-            BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args, new DebugInProcessConfig());
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args, new DebugInProcessConfig());
 #else
-            BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
 #endif
-        }
-    }
-}
