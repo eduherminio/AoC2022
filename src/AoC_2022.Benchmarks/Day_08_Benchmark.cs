0a286ce254feee399ef36d2e7a9281044e65eaa7
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-12-09 02:24:19 +0100

    Day 8 part 2: cache ranges, avoid checking borders and delay range calculation

---
+++ src/AoC_2022.Benchmarks/Day_08_Benchmark.cs
@@ -0,0 +1,37 @@
+﻿/*
+ *  |              Method |     Mean |     Error |    StdDev |   Median | Ratio | RatioSD |      Gen0 |     Gen1 | Allocated | Alloc Ratio |
+ *  |-------------------- |---------:|----------:|----------:|---------:|------:|--------:|----------:|---------:|----------:|------------:|
+ *  |               Part1 | 5.682 ms | 0.1125 ms | 0.1539 ms | 5.621 ms |  1.00 |    0.00 | 3210.9375 |        - |   6.42 MB |        1.00 |
+ *  | Part1_ReusingRanges | 8.519 ms | 0.7437 ms | 2.1930 ms | 7.006 ms |  2.00 |    0.22 | 2929.6875 | 132.8125 |    5.9 MB |        0.92 |
+ *
+ 9  |         Method |      Mean |     Error |    StdDev | Ratio | RatioSD |       Gen0 |     Gen1 | Allocated | Alloc Ratio |
+ 9  |--------------- |----------:|----------:|----------:|------:|--------:|-----------:|---------:|----------:|------------:|
+ 9  |          Part2 |  9.654 ms | 0.1658 ms | 0.1470 ms |  1.00 |    0.00 |  3578.1250 | 312.5000 |   7.31 MB |        1.00 |
+ 9  | Part2_Original | 19.657 ms | 0.3740 ms | 0.4002 ms |  2.04 |    0.04 | 13343.7500 |        - |  26.65 MB |        3.64 |
+ */
+
+using BenchmarkDotNet.Attributes;
+
+namespace AoC_2022.Benchmarks;
+
+public class Day_08_Part1 : BaseDayBenchmark
+{
+    private readonly Day_08 _problem = new();
+
+    [Benchmark(Baseline = true)]
+    public async Task<string> Part1() => await _problem.Solve_1();
+
+    [Benchmark]
+    public async Task<string> Part1_ReusingRanges() => await _problem.Solve_1_ReusingRanges();
+}
+
+public class Day_08_Part2 : BaseDayBenchmark
+{
+    private readonly Day_08 _problem = new();
+
+    [Benchmark(Baseline = true)]
+    public async Task<string> Part2() => await _problem.Solve_2();
+
+    [Benchmark]
+    public async Task<string> Part2_Original() => await _problem.Solve_2_Original();
+}
--- src/AoC_2022/Day_08.cs
+++ src/AoC_2022/Day_08.cs
@@ -47,6 +47,145 @@ public class Day_08 : BaseDay
 
         int visible = int.MinValue;
 
+        var leftRanges = new Dictionary<int, List<int>>(maxX);
+        var rightRanges = new Dictionary<int, List<int>>(maxX);
+        var upRanges = new Dictionary<int, List<int>>(maxY);
+        var downRanges = new Dictionary<int, List<int>>(maxY);
+
+        for (int y = 1; y < maxY - 1; ++y)
+        {
+            for (int x = 1; x < maxX - 1; ++x)
+            {
+                var point = _input[y][x];
+
+                if (!leftRanges.TryGetValue(x, out var rangeLeft))
+                {
+                    rangeLeft = leftRanges[x] = Enumerable.Range(0, x).Reverse().ToList();
+                }
+
+                var left = rangeLeft
+                    .TakeWhile((customX, index) =>
+                        (_input[y][customX] < point
+                            && (index == 0 || _input[y][rangeLeft[index - 1]] < point))
+                        || index == 0
+                        || _input[y][rangeLeft[index - 1]] < point)
+                    .Count();
+
+                if (left == 0) continue;
+
+                if (!rightRanges.TryGetValue(x, out var rangeRight))
+                {
+                    rangeRight = rightRanges[x] = Enumerable.Range(x + 1, maxX - x - 1).ToList();
+                }
+
+                var right = rangeRight
+                    .TakeWhile((customX, index) =>
+                        (_input[y][customX] < point
+                            && (index == 0 || _input[y][rangeRight[index - 1]] < point))
+                        || index == 0
+                        || _input[y][rangeRight[index - 1]] < point)
+                    .Count();
+
+                if (right == 0) continue;
+
+                if (!upRanges.TryGetValue(y, out var rangeUp))
+                {
+                    rangeUp = upRanges[y] = Enumerable.Range(0, y).Reverse().ToList();
+                }
+
+                var up = rangeUp
+                    .TakeWhile((customY, index) =>
+                        (_input[customY][x] < point
+                            && (index == 0 || _input[rangeUp[index - 1]][x] < point))
+                        || index == 0
+                        || _input[rangeUp[index - 1]][x] < point)
+                    .Count();
+
+                if (up == 0) continue;
+
+                if (!downRanges.TryGetValue(y, out var rangeDown))
+                {
+                    rangeDown = downRanges[y] = Enumerable.Range(y + 1, maxY - y - 1).ToList();
+                }
+
+                var down = rangeDown
+                    .TakeWhile((customY, index) =>
+                        (_input[customY][x] < point
+                            && (index == 0 || _input[rangeDown[index - 1]][x] < point))
+                        || index == 0
+                        || _input[rangeDown[index - 1]][x] < point)
+                    .Count();
+
+                var product = left * right * up * down;
+
+                if (product > visible)
+                {
+                    visible = product;
+                }
+            }
+        }
+
+        return new($"{visible}");
+    }
+
+    public ValueTask<string> Solve_1_ReusingRanges()
+    {
+        var maxX = _input[0].Count;
+        var maxY = _input.Count;
+
+        int visible = (2 * maxX) + (2 * maxY) - 4;
+
+        var leftRanges = new Dictionary<int, List<int>>(maxX);
+        var rightRanges = new Dictionary<int, List<int>>(maxX);
+        var upRanges = new Dictionary<int, List<int>>(maxY);
+        var downRanges = new Dictionary<int, List<int>>(maxY);
+
+        for (int y = 1; y < maxY - 1; ++y)
+        {
+            for (int x = 1; x < maxX - 1; ++x)
+            {
+                var point = _input[y][x];
+
+                if (!leftRanges.TryGetValue(x, out var rangeLeft))
+                {
+                    rangeLeft = leftRanges[x] = Enumerable.Range(0, x).ToList();
+                }
+
+                if (!upRanges.TryGetValue(y, out var rangeUp))
+                {
+                    rangeUp = upRanges[y] = Enumerable.Range(0, y).ToList();
+                }
+
+                if (!rightRanges.TryGetValue(x, out var rangeRight))
+                {
+                    rangeRight = rightRanges[x] = Enumerable.Range(x + 1, maxX - x - 1).ToList();
+                }
+
+                if (!downRanges.TryGetValue(y, out var rangeDown))
+                {
+                    rangeDown = downRanges[y] = Enumerable.Range(y + 1, maxY - y - 1).ToList();
+                }
+
+                if (rangeLeft.Select(customX => _input[y][customX]).All(value => value < point)
+                    || rangeRight.Select(customX => _input[y][customX]).All(value => value < point)
+                    || rangeUp.Select(customY => _input[customY][x]).All(value => value < point)
+                    || rangeDown.Select(customY => _input[customY][x]).All(value => value < point))
+                {
+                    ++visible;
+                }
+            }
+        }
+
+        return new($"{visible}");
+    }
+
+    public ValueTask<string> Solve_2_Original()
+    {
+        var maxX = _input[0].Count;
+        var maxY = _input.Count;
+
+        int visible = int.MinValue;
+
         for (int y = 0; y < maxY; ++y)
         {
             for (int x = 0; x < maxX; ++x)
